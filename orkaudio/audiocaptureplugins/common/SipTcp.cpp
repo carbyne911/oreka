c8e15f788e1e90602eb8f5749e9fc390e19764cb
oleksandrb <oleksandrb@carbyne.com>
2024-09-27 17:09:58 +0300

    Fix line endings

--- orkaudio/audiocaptureplugins/common/SipTcp.cpp
+++ orkaudio/audiocaptureplugins/common/SipTcp.cpp
@@ -22,6 +22,7 @@
 
 static LoggerPtr s_sipTcpPacketLog = Logger::getLogger("packet.tcpsip");
 
+// ============================================================
 
 static char* memFindAfter(const char* toFind, char* start, char* stop)
 {
@@ -124,6 +125,7 @@ void SipTcpStream::AddTcpPacket(u_char *pBuffer, int packetLen)
 {
 	m_tcpBuffer->Add(pBuffer, packetLen);
 }
+
 
 extern bool TrySipInvite(EthernetHeaderStruct* ethernetHeader, IpHeaderStruct* ipHeader, UdpHeaderStruct *udpHeader, u_char* payload, u_char* packetEnd, u_char* interfaceName);
 extern bool TrySip200Ok(EthernetHeaderStruct* ethernetHeader, IpHeaderStruct* ipHeader, UdpHeaderStruct *udpHeader, u_char* payload, u_char* packetEnd);
